7647b7dbabea5ebe53f8d1fc219e8d01197cf240
Y.K <powdersugar828828@gmail.com>
2022-05-01 23:59:21 +0900

    feat: リッチリザルトテストのリンクを追加して。

--- package/src/App.tsx
+++ package/src/App.tsx
@@ -1,9 +1,13 @@
 import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 import EditArea from '~/components/EditStructuredData/EditArea';
 
 function App() {
   return (
-    <Container>
+    <Container sx={{ mt: 2 }}>
+      <Link href="https://search.google.com/test/rich-results" underline="hover" target="_blank" rel="noopener">
+        構造化データのテストを行う
+      </Link>
       <EditArea />
     </Container>
   );
