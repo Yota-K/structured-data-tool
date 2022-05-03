fd952c17c263cbf932b2f7392c8d6b6f90510f9f
Y.K <powdersugar828828@gmail.com>
2022-05-04 02:57:59 +0900

    refactor: load.phpのリファクタ。

--- load.php
+++ load.php
@@ -9,53 +9,53 @@
   License:
  */
 
-   // 外部からPHPが直接実行された時は処理を終了する
-  // ABSPATH・・・WordPressディレクトリへの絶対パス
-  if (!defined('ABSPATH')) exit;
+// 外部からPHPが直接実行された時は処理を終了する
+// ABSPATH・・・WordPressディレクトリへの絶対パス
+if (!defined('ABSPATH')) exit;
 
-  require_once(__DIR__ . '/structured-data-tool.php');
-  require_once(__DIR__ . '/custom-field.php');
+require_once(__DIR__ . '/structured-data-tool.php');
+require_once(__DIR__ . '/custom-field.php');
 
-  // プラグインが有効化されたときに実行される処理
-  // MEMO: StructuredDataTool classに処理を書くとテーブルの作成が行われなかった
-  // ファイルを分割すると処理が実行されなくなる
-  function structured_data_tool_install()
-  {
-    global $wpdb;
+// プラグインを有効化したときに実行される処理
+function structured_data_tool_install()
+{
+  global $wpdb;
 
-    $db_version= '1.0';
-    $table_name = $wpdb->prefix . StructuredDataTool::TABLE_NAME;
+  $db_version= '1.0';
+  $table_name = $wpdb->prefix . StructuredDataTool::TABLE_NAME;
 
-    // テーブルが存在しない時は作成する
-    if (is_null($wpdb->get_var("SHOW TABLES LIKE '" .$table_name. "'"))) {
-      $sql = "CREATE TABLE ".$table_name." (
-        `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-        `post_name` varchar(255) CHARACTER SET utf8 NOT NULL,
-        `post_type` varchar(255) CHARACTER SET utf8 NOT NULL,
-        `value` tinyint(1) NOT NULL,
-        PRIMARY KEY  (id)
-      ) " .$wpdb->get_charset_collate(). ";";
+  // テーブルが存在しない時は作成する
+  if (is_null($wpdb->get_var("SHOW TABLES LIKE '" . $table_name . "'"))) {
+    $sql = "CREATE TABLE " . $table_name . " (
+      `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+      `post_name` varchar(255) CHARACTER SET utf8 NOT NULL,
+      `post_type` varchar(255) CHARACTER SET utf8 NOT NULL,
+      `value` tinyint(1) NOT NULL,
+      PRIMARY KEY  (id)
+    ) " . $wpdb->get_charset_collate() . ";";
 
-      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-      dbDelta($sql); 
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql); 
 
-      add_option('my_db_version', $db_version);
+    // データベースオプションを作成
+    // 登録したオプションはwp_optionsテーブル内に保存される
+    add_option('structured_data_tool', $db_version);
 
-      // 初期データの投入
-      StructuredDataTool::insert_init_data($wpdb, $table_name);
-    }
+    // 初期データの投入
+    StructuredDataTool::insert_init_data($wpdb, $table_name);
   }
-  register_activation_hook( __FILE__, 'structured_data_tool_install');
+}
+register_activation_hook(__FILE__, 'structured_data_tool_install');
 
-  // プラグインが削除されるときに実行される処理
-  function structured_data_tool_uninstall() 
-  {
-    global $wpdb;
+// プラグインが削除されるときに実行される処理
+function structured_data_tool_uninstall() 
+{
+  global $wpdb;
 
-    delete_option('my_db_version');
-    $table_name = $wpdb->prefix . StructuredDataTool::TABLE_NAME;
+  delete_option('structured_data_tool');
+  $table_name = $wpdb->prefix . StructuredDataTool::TABLE_NAME;
 
-    $sql = "DROP TABLE IF EXISTS $table_name";
-    $wpdb->query($sql);
-  }
-  register_uninstall_hook(__FILE__, 'structured_data_tool_uninstall');
+  $sql = "DROP TABLE IF EXISTS $table_name";
+  $wpdb->query($sql);
+}
+register_uninstall_hook(__FILE__, 'structured_data_tool_uninstall');
