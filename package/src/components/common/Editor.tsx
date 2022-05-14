fb9c7e30fb414d2ac409d964d157f0f5300879a1
Y.K <powdersugar828828@gmail.com>
2022-05-15 00:16:55 +0900

    feat: draft.jsで入力したHTMLを文字列に変換する処理入れて

--- package/src/components/common/Editor.tsx
+++ package/src/components/common/Editor.tsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
-import { EditorState } from 'draft-js';
+import { EditorState, convertToRaw } from 'draft-js';
 import { Editor as WysiwygEditor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-export default function Editor() {
+const Editor: React.FC = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const onEditorStateChange = (value: EditorState) => {
     setEditorState(value);
   };
+
+  // draft.jsで入力したHTMLを文字列に変換する
+  const rawContentState = convertToRaw(editorState.getCurrentContent());
+
   return (
     <div>
       <WysiwygEditor editorState={editorState} onEditorStateChange={onEditorStateChange} />
+      {draftToHtml(rawContentState)}
     </div>
   );
-}
+};
+export default Editor;
